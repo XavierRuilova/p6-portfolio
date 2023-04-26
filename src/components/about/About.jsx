4b773bff7cb92314ecc54a237afdecaaa49f7842
xavier ruilova <114267639+XavierRuilova@users.noreply.github.com>
2023-04-26 12:50:43 -0500

    description added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,21 +17,27 @@ const About = () => {
 				<span className="detail-title">
 					Perfil
 				</span>
-				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis itaque possimus quos dolores at non eligendi quod doloribus vitae laboriosam, asperiores enim rem molestiae, vel amet facilis consequuntur modi tempore?</p>
+				<p>Soy Ingeniero Informático de gestión, apasionado por las lenguas extranjeras me dediqué a aprender varios idiomas como: inglés, francés, italiano, mandarín y árabe. Luego surgió con los avances surgieron varias ideas que necesiaba plasmar por lo cual decidí convertirme en desarrollador web Full Stack. Los lenguajes de programación son otros idiomas agregados a mi passion ya que son la forma de comunicarse en la informática.</p>
 			</div>
 
 			<div className="column">
 				<span className="detail-title">
 					Ubicación
 				</span>
-				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis itaque possimus quos dolores at non eligendi quod doloribus vitae laboriosam, asperiores enim rem molestiae, vel amet facilis consequuntur modi tempore?</p>
+				<p>Guayaquil, Ecuador</p>
 			</div>
 
 			<div className="column">
 				<span className="detail-title">
 					Intereses
 				</span>
-				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis itaque possimus quos dolores at non eligendi quod doloribus vitae laboriosam, asperiores enim rem molestiae, vel amet facilis consequuntur modi tempore?</p>
+				{/* <p>Lenguas y civilizaciones extranjeras</p> */}
+				<ul style={{textAlign:'initial'}}>
+					<li>Lenguas, culturas y civilizaciones extranjeras</li>
+					<li>Desarrollo web</li>
+					<li>Meditación</li>
+					<li>Artes marciales</li>
+				</ul>
 			</div>
 		</div>
 	</div>
